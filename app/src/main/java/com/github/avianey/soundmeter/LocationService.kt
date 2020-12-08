4246879bb25d19ca91e3f0b4e77fc74312c97bd7
NicolasJeannette <44873578+NicolasJeannette@users.noreply.github.com>
2020-12-08 13:22:49 +0100

    Récupération

---
+++ app/src/main/java/com/github/avianey/soundmeter/LocationService.kt
@@ -0,0 +1,164 @@
+package com.github.avianey.soundmeter
+
+import android.annotation.SuppressLint
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.*
+import android.location.Criteria
+import android.location.Location
+import android.location.LocationManager
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import androidx.preference.PreferenceManager
+import io.reactivex.subjects.BehaviorSubject
+
+class LocationService: Service() {
+
+    companion object {
+        const val LOCATION_ACTION = "com.github.avianey.soundmeter.LOCATION_ACTION"
+        val EMPTY_LOCATION = Location("")
+        const val NOTIFICATION_TAG_SPEED_THRESHOLD = "123"
+        var COUNT = 0
+
+        fun startOrStop(context: Context) {
+            Intent(context, LocationService::class.java).let { intent ->
+                if (isRunning) {
+                    context.stopService(intent)
+                } else {
+                    context.startService(intent)
+                }
+            }
+        }
+
+        var isRunning = false
+            private set
+
+        var locationObservable = BehaviorSubject.createDefault(EMPTY_LOCATION)
+    }
+
+    private lateinit var locationManager: LocationManager
+    private lateinit var locationReceiver: BroadcastReceiver
+    private val speedThresholdListener = SharedPreferences.OnSharedPreferenceChangeListener { _, key ->
+        if (key == SoundMeterSettings.SETTING_SPEED) {
+            locationObservable.value?.let {  location ->
+                if (location !== EMPTY_LOCATION) {
+                    checkSpeed(this, location)
+                }
+            }
+        }
+    }
+
+    // region lifecycle
+
+    override fun onBind(intent: Intent?): IBinder? {
+        throw IllegalStateException("Should not be bound")
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+        locationReceiver = LocationBroadcastReceiver()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        registerReceivers()
+        requestLocationUpdates()
+        startForeground(42, getPersistentNotification())
+        isRunning = true
+        PreferenceManager.getDefaultSharedPreferences(this)
+            .registerOnSharedPreferenceChangeListener(speedThresholdListener)
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopLocationUpdates()
+        unregisterReceivers()
+        stopForeground(true)
+        isRunning = false
+        PreferenceManager.getDefaultSharedPreferences(this)
+            .unregisterOnSharedPreferenceChangeListener(speedThresholdListener)
+    }
+
+    // endregion
+
+    @SuppressLint("MissingPermission")
+    private fun requestLocationUpdates() {
+        locationManager.requestLocationUpdates(
+            SoundMeterActivity.LOCATION_UPDATE_MS,
+            SoundMeterActivity.LOCATION_UPDATE_RADIUS,
+            Criteria().apply {
+                accuracy = Criteria.ACCURACY_FINE
+            }, getLocationPendingIntent())
+    }
+
+    private fun stopLocationUpdates() {
+        locationManager.removeUpdates(getLocationPendingIntent())
+    }
+
+    private fun registerReceivers() {
+        registerReceiver(locationReceiver, IntentFilter(LOCATION_ACTION))
+    }
+
+    private fun unregisterReceivers() {
+        unregisterReceiver(locationReceiver)
+    }
+
+    private fun getPersistentNotification() =
+        NotificationCompat.Builder(this, SoundMeterApplication.NOTIFICATION_CHANNEL)
+            .setContentTitle(getString(R.string.notification_title))
+            .setContentIntent(getActivityPendingIntent())
+            .setColorized(true)
+            .setColor(resources.getColor(R.color.purple_500))
+            .build()
+
+    private fun getLocationPendingIntent() =
+        PendingIntent.getBroadcast(this, 0,
+            Intent(LOCATION_ACTION), PendingIntent.FLAG_UPDATE_CURRENT)
+
+    private fun getActivityPendingIntent() =
+        PendingIntent.getActivity(this, 0,
+            Intent(this, SoundMeterActivity::class.java),
+            PendingIntent.FLAG_CANCEL_CURRENT)
+
+
+    private inner class LocationBroadcastReceiver: BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent?) {
+            intent?.extras?.getParcelable<Location>(LocationManager.KEY_LOCATION_CHANGED)?.let { location ->
+                locationObservable.onNext(location)
+                checkSpeed(context, location)
+            }
+        }
+    }
+
+    private fun checkSpeed(context: Context, location: Location) {
+        val threshold =
+            PreferenceManager.getDefaultSharedPreferences(context)
+                .getInt(
+                    SoundMeterSettings.SETTING_SPEED,
+                    resources.getInteger(R.integer.speed_default)
+                )
+        if (location.speed > threshold) {
+            // threshold reached
+            (context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager)
+                .notify(
+                    NOTIFICATION_TAG_SPEED_THRESHOLD, COUNT++,
+                    NotificationCompat.Builder(context, SoundMeterApplication.NOTIFICATION_CHANNEL)
+                        .setSmallIcon(R.drawable.ic_launcher_foreground)
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        .setContentTitle(
+                            getString(
+                                R.string.notification_speed_title,
+                                location.speed.toString(),
+                                threshold.toString()
+                            )
+                        )
+                        .setContentIntent(getActivityPendingIntent())
+                        .build()
+                )
+        }
+    }
+
+
+}
--- app/src/main/java/com/github/avianey/soundmeter/SoundMeterActivity.kt
+++ app/src/main/java/com/github/avianey/soundmeter/SoundMeterActivity.kt
@@ -1,11 +1,56 @@
 package com.github.avianey.soundmeter
 
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.Intent
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.location.Location
 import android.os.Bundle
+import android.os.Vibrator
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.preference.PreferenceManager
+import io.reactivex.disposables.Disposable
+import io.reactivex.functions.Consumer
 
 class SoundMeterActivity: AppCompatActivity() {
 
-    private var popupDisplayed = false
+    companion object {
+        const val POPUP_DISPLAYED = "popupAlreadyDisplayed"
+        const val TAG_PERMISSION_FRAGMENT = "permissionDialogFragment"
+        const val REQUEST_CODE_PERMISSION = 1
+        const val LOCATION_UPDATE_MS = 10_000L
+        const val LOCATION_UPDATE_RADIUS = 100f
+    }
+
+    private var popupAlreadyDisplayed = false
+
+    private lateinit var startStopBtn: Button
+    private lateinit var coordinatesView: TextView
+    private lateinit var speedView: TextView
+
+    val locationConsumer = Consumer<Location> { location ->
+        coordinatesView.text = getString(R.string.location_template,
+            String.format("%.6f", location.latitude),
+            String.format("%.6f", location.longitude))
+
+        speedView.text = getString(R.string.speed_template,
+            String.format("%.2f", location.speed ?: 0))
+
+        /*
+        PreferenceManager.getDefaultSharedPreferences(this).edit()
+            .putInt(SoundMeterSettings.SETTING_SPEED, 60)
+            .apply()
+         */
+    }
+
+    var locationDisposable: Disposable? = null
+
+    // region lifecycle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,16 +62,71 @@ class SoundMeterActivity: AppCompatActivity() {
                 setImageResource(R.mipmap.ic_launcher)
             })
         })*/
-        if (savedInstanceState?.getBoolean("popupAlreadyDisplayed") != true) {
-            // afficher popup
-
-            popupDisplayed = true
+        startStopBtn = findViewById(R.id.start)
+        startStopBtn.setOnClickListener {
+            LocationService.startOrStop(this)
+            syncUI() // TODO
         }
+        popupAlreadyDisplayed = savedInstanceState?.getBoolean(POPUP_DISPLAYED) ?: false
+        coordinatesView = findViewById(R.id.coordinates)
+        speedView = findViewById(R.id.speed)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PERMISSION_GRANTED) {
+            if (supportFragmentManager.findFragmentByTag(TAG_PERMISSION_FRAGMENT) == null
+                && !popupAlreadyDisplayed) {
+                supportFragmentManager
+                    .beginTransaction()
+                    .add(PermissionPopupFragment(), TAG_PERMISSION_FRAGMENT)
+                    .commit()
+                popupAlreadyDisplayed = true
+            }
+        }
+        syncUI()
+        locationDisposable = LocationService.locationObservable.subscribe(locationConsumer)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        locationDisposable?.dispose()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putBoolean("popupAlreadyDisplayed", popupDisplayed)
+        outState.putBoolean(POPUP_DISPLAYED, popupAlreadyDisplayed)
+    }
+
+    // endregion
+
+    // region menu
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_sound_meter, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.open_settings -> {
+                startActivity(Intent(this, SoundMeterSettings::class.java))
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    // endregion
+
+    private fun syncUI() {
+        if (LocationService.isRunning) {
+            startStopBtn.text = getString(R.string.btn_stop)
+        } else {
+            startStopBtn.text = getString(R.string.btn_start)
+        }
+        startStopBtn.isEnabled =
+                ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PERMISSION_GRANTED
     }
 
 }
